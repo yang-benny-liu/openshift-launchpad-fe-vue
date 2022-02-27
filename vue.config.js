1bbefe809ed4e680a437e4a803f39d4f719d0f10
yang-benny-liu <42212764+yang-benny-liu@users.noreply.github.com>
2022-02-27 11:01:51 +0800

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -5,6 +5,7 @@ module.exports = {
   ],
   runtimeCompiler: true,
   devServer: {
+    proxy: 'http://openshift-launchpad-fe-vue.appdemo.svc.cluster.local:8000'
     disableHostCheck: true,
   }
 };
